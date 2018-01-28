079bb40d6f04391129658c3af8f8d94416810013
Armando Pesenti Gritti <arpesenti@gmail.com>
2018-01-28 16:07:54 +0000

    fix random job

--- jobs/sonnen.rb
+++ jobs/sonnen.rb
@@ -1,6 +1,6 @@
 SCHEDULER.every '2s' do
   send_event('charge', { value: rand(100) })
-  send_event('battery-power', { current_value: rand(100) })
-  send_event('consumption', { current_value: rand(100) })
-  send_event('production', { current_value: rand(100) }) 
+  send_event('battery-power', { current: rand(100) })
+  send_event('consumption', { current: rand(100) })
+  send_event('production', { current: rand(100) }) 
 end
