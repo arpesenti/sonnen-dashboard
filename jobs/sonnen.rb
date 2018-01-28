979b39d4eb7761fd762288b9839cfd3740ff35a9
Armando Pesenti Gritti <arpesenti@gmail.com>
2018-01-28 16:47:03 +0000

    catch exception

--- jobs/sonnen.rb
+++ jobs/sonnen.rb
@@ -5,10 +5,14 @@ BATTERY_IP = '192.168.1.110'
 uri = URI('http://' + BATTERY_IP + ':8080/api/v1/status')
 
 SCHEDULER.every '2s' do
-  response = Net::HTTP.get(uri)
-  json = JSON.parse(response)
-  send_event('charge', { value: json.USOC })
-  send_event('battery-power', { current: json.Pac_total_W })
-  send_event('consumption', { current: json.Consumption_W })
-  send_event('production', { current: json.GridFeedIn }) 
+  begin 
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+    send_event('charge', { value: json['USOC'] })
+    send_event('battery-power', { current: json['Pac_total_W'] })
+    send_event('consumption', { current: json['Consumption_W'] })
+    send_event('production', { current: json['GridFeedIn'] }) 
+  rescue StandardError
+    false
+  end
 end
